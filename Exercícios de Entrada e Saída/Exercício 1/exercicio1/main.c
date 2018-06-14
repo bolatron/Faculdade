82e6c51d387aff704bcc380ca027cb71ac9fc87d
ArthurGM18 <40254576+ArthurGM18@users.noreply.github.com>
2018-06-13 23:44:02 -0300

    Update main.c

--- Exercícios de Entrada e Saída/Exercício 1/exercicio1/main.c
+++ Exercícios de Entrada e Saída/Exercício 1/exercicio1/main.c
@@ -5,7 +5,7 @@ int main()
 {
     int n1,n2,n3;
     printf("Digite 3 valores inteiros: ");
-    scanf("%d%d%d",&n1,&n2,&n3);
+    scanf("%d%d%d",&n1,&n2,&n3);                                   //SOMA TRÊS VALORES INTEIROS
     printf("A soma dos valores informados eh: %d\n",n1+n2+n3);
     system("pause");
     return 0;
